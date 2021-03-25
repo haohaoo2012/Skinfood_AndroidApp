ac45b73e202a30c1e7da6b74db0c733f80798dc7
haohaoo2012 <73508026+haohaoo2012@users.noreply.github.com>
2021-03-25 10:10:17 +0700

    Initial commit

---
+++ app/src/main/java/nguyendoduchao/ueh/edu/apptest2/Productdetails.java
@@ -0,0 +1,14 @@
+package nguyendoduchao.ueh.edu.apptest2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Productdetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_productdetails);
+    }
+}
